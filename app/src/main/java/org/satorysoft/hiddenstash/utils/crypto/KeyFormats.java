99f2df9534fa33b0a7562fd3f31e3e8b750584c3
Viacheslav Okolitiy <okolitiy.viacheslav@gmail.com>
2016-09-25 19:13:47 +0300

    Started new iteration

---
+++ app/src/main/java/org/satorysoft/hiddenstash/utils/crypto/KeyFormats.java
@@ -0,0 +1,10 @@
+package org.satorysoft.hiddenstash.utils.crypto;
+
+/**
+ * Created by viacheslavokolitiy on 25.09.2016.
+ */
+
+public enum KeyFormats {
+    PEM,
+    DER
+}
---
+++ app/src/main/java/org/satorysoft/hiddenstash/utils/crypto/KeyTypes.java
@@ -0,0 +1,10 @@
+package org.satorysoft.hiddenstash.utils.crypto;
+
+/**
+ * Created by viacheslavokolitiy on 25.09.2016.
+ */
+
+public enum KeyTypes {
+    PRIVATE_KEY,
+    PUBLIC_KEY
+}
